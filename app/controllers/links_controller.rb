ad16080e81b5a421c250ab36e3baaeae458633b9
Angelo Ashmore <angelo@neutyp.com>
2008-12-07 11:30:07 +0000

    Implementing BackgrounDRb into the link retrieving process. It works, but I want to test it on the server for speed.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -10,67 +10,14 @@ class LinksController < ApplicationController
     when :get
       ###
     
-    when :post
-      require 'net/http'
-      require 'hpricot'
-      require 'open-uri'
-      
+    when :post      
       @category = Category.find(params[:category][:id]) unless params[:category][:id].length < 1
       
       number_of_links = 0
       
       params[:link][:uri].each_line do |uri|
-        uri = uri.strip.chomp
-        
-        if uri.length > 0
-          scheme = URI.parse(uri).scheme
-          if scheme == nil
-            uri = "http://" + uri
-            scheme = "http"
-          end
-          if scheme.downcase != "http"
-            flash[:error] = "Sorry, only HTTP URIs are currently supported."
-            return redirect_to session[:referrer] || '/'
-          end
-          uri = URI.parse(uri)
-
-          response = Net::HTTP.new(uri.host).request_head((uri.path.length > 0 ? uri.path : "index"))
-
-          if response == 404
-            flash[:error] = "Looks like that page doesn't exist!"
-            return redirect_to session[:referrer] || '/'
-          else
-            @domain = Domain.find(:first,
-                                  :conditions => ['scheme = ? and domain = ?', uri.scheme, uri.host])
-            add_domain(uri) if !@domain
-
-            @link = Link.new
-            @link.member_id = @master_member.id
-            @link.domain_id = @domain.id
-            @link.category_id = @category ? @category.id : nil
-            # images won't return a title, so filename is used
-            # hopefully we can come up with a better method
-            begin
-              document_html = Hpricot(open(uri, "User-Agent" => "theurld"))
-              title = document_html.search("title").html.strip
-              @link.title = title.length > 0 ? title : File.basename(uri.to_s)
-            rescue Exception => e
-              log_error(e)
-              @link.title = File.basename(uri.to_s)
-            end
-
-            @link.title = File.basename(uri.to_s) unless @link.title.length > 0
-            @link.uri = uri.to_s
-            @link.path = uri.path.to_s
-            @link.code = generate_code
-            
-            if @link.save
-              @domain.update_attribute('number_of_links', @domain.number_of_links + 1)
-              @link.category.update_attribute('number_of_links', @link.category.number_of_links + 1) if @category
-              number_of_links += 1
-            end
-          end
-        end
+        MiddleMan.worker(:links_worker).async_new(:arg => {:uri => uri, :master_member_id => @master_member.id, :category_id => (@category ? @category.id : nil), :number_of_links => number_of_links})
+        number_of_links += 1
       end
       
       if number_of_links < 1
@@ -82,75 +29,6 @@ class LinksController < ApplicationController
       end
       
       redirect_to session[:referrer] || '/' and return
-    end
-  end
-  
-  private
-  
-  def add_domain(uri)
-    @domain = Domain.new
-    
-    begin
-      document_html = Hpricot(open(uri.host, "User-Agent" => "theurld"))
-      title = document_html.search("title").html.strip
-      @domain.title = title.length > 0 ? title : File.basename(uri.to_s)
-    rescue Exception => e
-      log_error(e)
-      @domain.title = File.basename(uri.to_s)
-    end
-    
-    @domain.title = uri.host unless @domain.title.length > 0
-    @domain.scheme = uri.scheme
-    @domain.domain = uri.host
-    @domain.save
-    
-    favicon_location  = File.join(FAVICONS_LOCATION, "#{@domain.id}")
-    
-    begin
-      if Net::HTTP.new(@domain.domain).request_head(("favicon.ico")).to_s != "404"
-        require 'RMagick'
-      
-        thread = Thread.new do
-          Net::HTTP.start(@domain.domain) { |http|
-            resp = http.get("/favicon.ico")
-            open(favicon_location + ".ico", "w") { |favicon|
-              favicon.write(resp.body)
-            }
-          }
-    
-          original_file = Magick::Image.read(favicon_location + ".ico").first.resize_to_fit(16,16)
-          # this doesn't work apparently
-          # transparent GIFs are given a black background
-          # 
-          # original_file.background_color = 'none'
-          original_file.write(favicon_location + ".gif")
-    
-          @domain.update_attribute('favicon', 1)
-        end
-        thread.join(2)
-      end
-    rescue Exception => e
-      log_error(e)
-      # Angelo Ashmore, 11/10/08: 
-      # doesn't really matter if it fails to get the favicon
-      # but maybe there should be a way for it to try
-      # to get the favicon again when a user submits the same
-      # domain again and the favicon has yet to be retrieved
-    end
-    
-    File.delete(favicon_location + ".ico") if File.exists?(favicon_location + ".ico")
-  end
-  
-  def generate_code
-    chars = ("a".."z").to_a + ("1".."9").to_a
-    code = Array.new(5, '').collect{chars[rand(chars.size)]}.join
-    
-    # Loop this method if the generated code matches an already existing one
-    existing_code = Link.find_by_code(code)
-    if existing_code or FORBIDDEN_NAMES.include?(code)
-      generate_code and return false
-    else
-      code
     end
   end
   
